2533b4625f7058cedb75348f8c49dd507fa4676a
Emrik Ostling <emrik.ostling@elev.ga.ntig.se>
2020-02-20 15:47:37 +0100

    almost finished file upload sql

--- app.rb
+++ app.rb
@@ -1,41 +1,44 @@
+# frozen_string_literal: true
+
 require "slim"
 require "sinatra"
 require "sqlite3"
 require "bcrypt"
+require_relative "./model.rb"
 
 enable :sessions
 
-db = SQLite3::Database.new("db/users.db")
-db.results_as_hash = true
-
 before do
-  if request.path_info != "/" && !request.path_info.start_with?("/user/") && session[:login].nil?
-    redirect("/")
-  end
-end
+  # for testing
+  # session[:login] = "$2a$12$Wr1G1Y7elWcsdBPGJeUq0ut3AMsZZ/58jqhsC6Uxx2cz2Wn/wsMOS"
+  # session[:name] = "hej"
+  # session[:role] = "member"
 
+  redirect("/") if request.path_info != "/" && !request.path_info.start_with?("/user/") && session[:login].nil?
+end
 
 get("/") do
   if session[:login].nil?
+    # Not logged in redirects to the startpage
     slim(:start)
   else
+    # logged in
     redirect("/files/view")
   end
 end
 
 get("/files/view") do
-  user_id = db.execute("SELECT id FROM users WHERE password_digest = ?", session[:login])
-  file_id = db.execute("SELECT file_id FROM files_users WHERE user_id = ?", user_id[0]["id"])
-  slim(:"files/view", locals: {name: session[:name], user_id: user_id, file_id: file_id})
+  files = get_files(session[:login])
+  slim(:"files/view", locals: {name: session[:name], user_id: files[0], file_id: files[1]})
 end
 
 post("/files/create") do
+  # to-do: Export to model.rb
   user_id = db.execute("SELECT id FROM users WHERE password_digest = ?", session[:login])[0]["id"]
   filename = params[:file][:filename]
   file = params[:file][:tempfile]
   path = "files/#{user_id}/#{filename}"
   public_file = 0
-
   if params[:public]
     path = "./public/" + path
     public_file = 1
@@ -43,50 +46,44 @@ post("/files/create") do
     path = "./private/" + path
   end
 
-  unless Dir.exist?("private/files/#{user_id}")
-    Dir.mkdir "private/files/#{user_id}"
-  end
+  Dir.mkdir "private/files/#{user_id}" unless Dir.exist?("private/files/#{user_id}")
 
   File.open(path, "w+") do |f|
     f.write(file.read)
   end
-  hmm = db.execute("INSERT INTO files (date, path, public) VALUES (?, ?, ?);", Time.now.to_i, path, public_file)
-  p hmm
-  # params[:file][:type]
+  db.execute("INSERT INTO files (date, path, public) VALUES (?, ?, ?);", Time.now.to_i, path, public_file)
+  file_id = db.execute("SELECT id FROM files WHERE path = ?", path)[0]["id"]
+  db.execute("INSERT INTO files_users (file_id, user_id) VALUES (?, ?);", file_id, user_id)
+  category = db.execute("SELECT id FROM category WHERE name = ?", params[:file][:type])
+
+  db.execute("INSERT INTO category (name) VALUES (?);", params[:file][:type]) if category == []
+
+  category_id = db.execute("SELECT id FROM category WHERE name = ?", params[:file][:type])[0]["id"]
+
+  db.execute("INSERT INTO category_files (cat_id, file_id) VALUES (?, ?);", category_id, file_id)
   redirect("/files/view")
 end
 
 post("/user/register") do
-  password = params[:password]
-  name = params[:name]
-  password_digest = BCrypt::Password.create(password)
-  session[:login] = password_digest
-  session[:name] = name
-  begin
-    db.execute("INSERT INTO users (username, password_digest, role) VALUES (?, ?, 'member')", name, password_digest)
-  rescue StandardError => e
-    p e
-  end
+  session[:name] = params[:name]
+  session[:role] = "member"
+  session[:login] = user_register(params[:password], params[:name])
   redirect("/")
 end
 
 post("/user/login") do
   password = params[:password]
   name = params[:name]
-  password_digest = db.execute("SELECT password_digest,role FROM users WHERE username = ?", name)
-
-  session[:role] = password_digest[0]["role"] == "admin" ? "admin" : "member"
-
-  if password_digest != [] && BCrypt::Password.new(password_digest[0]["password_digest"]) == password
-    session[:login] = password_digest[0]["password_digest"]
+  user_info = user_login(password, name)
+  if user_info[:password_correct]
+    session[:login] = user_info[:password_digest]
     session[:name] = name
+    session[:role] = user_info[:role] == "admin" ? "admin" : "member"
   end
   redirect("/")
 end
 
 post("/user/logout") do
-  session[:role] = "member"
-  session[:login] = nil
-  session[:name] = nil
+  session.clear
   redirect("/")
 end
