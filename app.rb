ffc795a62b15f1af67e54465dcef5e879c8c4447
Emrik Ostling <emrik.ostling@elev.ga.ntig.se>
2020-03-05 15:54:07 +0100

    added email, cookies and started with delete

--- app.rb
+++ app.rb
@@ -4,85 +4,165 @@ require "slim"
 require "sinatra"
 require "sqlite3"
 require "bcrypt"
+require "securerandom"
+
 require_relative "./model.rb"
 
 enable :sessions
 
-before do
-  # for testing
-  # session[:login] = "$2a$12$Wr1G1Y7elWcsdBPGJeUq0ut3AMsZZ/58jqhsC6Uxx2cz2Wn/wsMOS"
-  # session[:name] = "hej"
-  # session[:role] = "member"
+development = true
+require "sinatra/reloader" if development
+also_reload "./model.rb"
+also_reload "./app.rb"
 
-  redirect("/") if request.path_info != "/" && !request.path_info.start_with?("/user/") && session[:login].nil?
+# for cookies
+require "rack/contrib"
+require "sinatra/cookies"
+use Rack::Cookies
+helpers Sinatra::Cookies
+
+# look into this later
+require "rack/protection"
+use Rack::Protection
+
+# https://github.com/kickstarter/rack-attack needs to be configured
+require "rack/attack"
+use Rack::Attack
+
+Rack::Attack.throttle("requests by ip", limit: 5, period: 2) do |request|
+  request.ip
+end
+
+# require "sinatra/rate-limiter"
+# enable :rate_limiter
+
+# for mail
+require "mail"
+
+require "parseconfig"
+config = ParseConfig.new("./config.properties")
+
+Mail.defaults do
+  delivery_method :smtp, {
+    address: config["email_server"],
+    port: 465,
+    user_name: config["email_user"],
+    password: config["email_password"],
+    authentication: :login,
+    ssl: true,
+    openssl_verify_mode: "none",
+  }
+end
+
+before do
+  if !development
+    session[:login] = "$2a$12$Wr1G1Y7elWcsdBPGJeUq0ut3AMsZZ/58jqhsC6Uxx2cz2Wn/wsMOS"
+    session[:name] = "hej"
+    session[:role] = "member"
+  else
+    # rate_limit
+  end
+
+  session[:error] = nil if request.post?
+
+  # if request.path_info != "/" && !request.path_info.start_with?("/user/") && session[:login].nil?
+  #   redirect("/")
+  # end
 end
 
 get("/") do
   if session[:login].nil?
+    if !request.cookies["remember_me"].nil? && request.cookies["remember_me"] != ""
+      session[:login] = request.cookies["remember_me"]
+      session[:name] = "test"
+      session[:role] = "member"
+      redirect("/files/view")
+    end
     # Not logged in redirects to the startpage
-    slim(:start)
+    slim(:start, locals: {error: session[:error]})
   else
-    # logged in
     redirect("/files/view")
   end
 end
 
 get("/files/view") do
-  files = get_files(session[:login])
-  slim(:"files/view", locals: {name: session[:name], user_id: files[0], file_id: files[1]})
+  file_ids = get_file_ids(session[:login])
+  files = get_files(file_ids)
+  slim(:"files/view", locals: {error: session[:error], name: session[:name], files: files[:files], public_files: files[:public_files]})
+end
+
+get("/private/files/:user_id/:file_name") do
+  if params[:user_id].to_i == get_user_id(session[:login])
+    send_file("./private/files/#{params[:user_id]}/#{params[:file_name]}")
+  else
+    halt 403, "Access Denied, logged in as #{get_user_id(session[:login])} expected #{params[:user_id]}"
+  end
+end
+
+get("/user/confirm_email/:key") do
+  if session[:confirm_email] == params[:key] && !session[:login].nil?
+    email_confirm(session[:login])
+    session[:confirm_email] = nil
+    redirect("/files/view")
+  else
+    "Wrong email or link, lol\nGo home: http://#{request.host_with_port}"
+  end
 end
 
 post("/files/create") do
-  # to-do: Export to model.rb
-  user_id = db.execute("SELECT id FROM users WHERE password_digest = ?", session[:login])[0]["id"]
-  filename = params[:file][:filename]
-  file = params[:file][:tempfile]
-  # path = "files/#{user_id}/#{filename}"
-  path = "files/#{user_id}"
-  public_file = 0
-  
-  if params[:public]
-    path = "public/" + path
-    public_file = 1
-  else
-    path = "private/" + path
-  end
+  file_upload(session[:login], params[:file], params[:public])
+  redirect("/files/view")
+end
 
-  Dir.mkdir path unless Dir.exist?(path)
-  path = "./#{path}/#{filename}"
-  File.open(path, "w+") do |f|
-    f.write(file.read)
-  end
-
-  db.execute("INSERT INTO files (date, path, public) VALUES (?, ?, ?);", Time.now.to_i, path, public_file)
-  file_id = db.execute("SELECT id FROM files WHERE path = ?", path)[0]["id"]
-  db.execute("INSERT INTO files_users (file_id, user_id) VALUES (?, ?);", file_id, user_id)
-  category = db.execute("SELECT id FROM category WHERE name = ?", params[:file][:type])
-
-  if category == []
-    db.execute("INSERT INTO category (name) VALUES (?);", params[:file][:type])
-    category = db.execute("SELECT id FROM category WHERE name = ?", params[:file][:type])
-  end
-
-  db.execute("INSERT INTO category_files (cat_id, file_id) VALUES (?, ?);", category[0]["id"], file_id)
+post("/files/delete") do
+  file_id = params[:file_id]
+  delete_file(file_id, session[:login])
   redirect("/files/view")
 end
 
 post("/user/register") do
+  digest = user_register(params[:password], params[:name], params[:email])
+
+  if digest.start_with?("ERROR: ")
+    session[:error] = digest[7..-1]
+    redirect("/")
+  else
+    session[:login] = digest
+  end
+
+  confirm_email_key = SecureRandom.urlsafe_base64
+  session[:confirm_email] = confirm_email_key
+
+  Mail.new(
+    to: params[:email].to_s,
+    from: "nasirforpresident2020@national.shitposting.agency",
+    subject: "Confirm your account at epic cloud site",
+    body: "Welcome to OUR site #{params[:name]}, where we steal you're data and sell it for profit\nSounds good?\nClick here to erase your suffering (by reading this mail you accept all our terms and conditions)\nhttp://#{request.host_with_port}/user/confirm_email/#{confirm_email_key}\n\nThanks for all the fish!",
+  ).deliver
+
   session[:name] = params[:name]
   session[:role] = "member"
-  session[:login] = user_register(params[:password], params[:name])
   redirect("/")
 end
 
 post("/user/login") do
   password = params[:password]
   name = params[:name]
+  remember_password = params[:remember]
   user_info = user_login(password, name)
   if user_info[:password_correct]
+    if remember_password
+      response.set_cookie("remember_me",
+        value: user_info["password_digest"],
+        expires: Time.now + 2_592_000, # One month from today
+        path: "/")
+    end
     session[:login] = user_info[:password_digest]
     session[:name] = name
     session[:role] = user_info[:role] == "admin" ? "admin" : "member"
+  else
+    # Maybe change
+    session[:error] = "incorrect login"
   end
   redirect("/")
 end
