1f40056077bc291ebce6af3d52dd392f97df80dc
Emrik Ostling <emrik.ostling@elev.ga.ntig.se>
2020-03-26 17:31:01 +0100

    Removed comments

--- public/script.js
+++ public/script.js
@@ -1,51 +1,34 @@
+// Taken from w3schools
 function sortTable(n, table) {
-  var rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0
-  switching = true
-  // Set the sorting direction to ascending:
-  dir = "asc"
-  /* Make a loop that will continue until
-  no switching has been done: */
+  let rows, i, x, y, shouldSwitch, switchcount = 0
+  let switching = true
+  let dirUp = true
   while (switching) {
-    // Start by saying: no switching is done:
     switching = false
     rows = table.rows
-    /* Loop through all table rows (except the
-    first, which contains table headers): */
     for (i = 1; i < (rows.length - 1); i++) {
-      // Start by saying there should be no switching:
       shouldSwitch = false
-      /* Get the two elements you want to compare,
-      one from current row and one from the next: */
       x = rows[i].getElementsByTagName("TD")[n]
       y = rows[i + 1].getElementsByTagName("TD")[n]
-      /* Check if the two rows should switch place,
-      based on the direction, asc or desc: */
-      if (dir == "asc") {
+      if (dirUp == true) {
         if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-          // If so, mark as a switch and break the loop:
           shouldSwitch = true
           break
         }
-      } else if (dir == "desc") {
+      } else if (dirUp == false) {
         if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-          // If so, mark as a switch and break the loop:
           shouldSwitch = true
           break
         }
       }
     }
     if (shouldSwitch) {
-      /* If a switch has been marked, make the switch
-      and mark that a switch has been done: */
       rows[i].parentNode.insertBefore(rows[i + 1], rows[i])
       switching = true
-      // Each time a switch is done, increase this count by 1:
       switchcount ++
     } else {
-      /* If no switching has been done AND the direction is "asc",
-      set the direction to "desc" and run the while loop again. */
-      if (switchcount == 0 && dir == "asc") {
-        dir = "desc"
+      if (switchcount == 0 && dirUp == true) {
+        dirUp = false
         switching = true
       }
     }
